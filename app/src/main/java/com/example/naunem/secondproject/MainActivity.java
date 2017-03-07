fb2ab21bbb96d91999f5c8fbda34ea31888243b6
at-nhanphan <naunemno1@gmail.com>
2017-03-07 23:19:23 +0700

    modified textview

--- app/src/main/java/com/example/naunem/secondproject/MainActivity.java
+++ app/src/main/java/com/example/naunem/secondproject/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.naunem.secondproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView text;
+    TextView branch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = (TextView) findViewById(R.id.text);
+        branch = (TextView) findViewById(R.id.branch2);
+
+        branch.setText("Ahihi");
     }
 }
