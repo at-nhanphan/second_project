db426dfdc6449e468e61a13e6811e08db1657d26
at-nhanphan <naunemno1@gmail.com>
2017-03-08 17:19:22 +0700

    done

--- app/src/main/java/com/example/naunem/secondproject/LoginActivity.java
+++ app/src/main/java/com/example/naunem/secondproject/LoginActivity.java
@@ -4,9 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
+import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 /**
@@ -18,6 +24,10 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     EditText username;
     EditText pass;
     Button btnLogin;
+    ImageView showPass;
+    TextView new_account;
+    private int passwordNotVisible = 1;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,12 +37,47 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         pass = (EditText) findViewById(R.id.edittext_pass);
         btnLogin = (Button) findViewById(R.id.login);
         btnLogin.setOnClickListener(this);
+        showPass = (ImageView) findViewById(R.id.showpass);
+        new_account = (TextView) findViewById(R.id.new_account);
+        new_account.setOnClickListener(this);
+//        pass.setTransformationMethod(new PasswordTransformationMethod());
+//        showPass.setOnTouchListener(new View.OnTouchListener() {
+//            @Override
+//            public boolean onTouch(View v, MotionEvent event) {
+//                if (MotionEvent.ACTION_DOWN == event.getAction()) {
+//                    Log.d("TAG", "onTouch: ");
+//                    pass.setTransformationMethod(null);
+//                    event.setAction(MotionEvent.ACTION_UP);
+//                } else {
+//                    pass.setTransformationMethod(new PasswordTransformationMethod());
+//                }
+//                return false;
+//            }
+//        });
+        showPass.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        Toast.makeText(this, "result : \n" + "username: " + username.getText().toString() + "\npassword: " + pass.getText().toString(), Toast.LENGTH_LONG).show();
-        Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-        startActivity(intent);
+        switch (v.getId()) {
+            case R.id.login:
+                Toast.makeText(this, "result : \n" + "username: " + username.getText().toString() + "\npassword: " + pass.getText().toString(), Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.showpass:
+                if(passwordNotVisible == 1) {
+                    pass.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                    passwordNotVisible = 0;
+                } else {
+                    pass.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    passwordNotVisible = 1;
+                }
+                pass.setSelection(pass.length());
+                break;
+            case R.id.new_account:
+                Intent intent1 = new Intent(this, RegisterActivity.class);
+                startActivity(intent1);
+        }
     }
 }
--- app/src/main/java/com/example/naunem/secondproject/RegisterActivity.java
+++ app/src/main/java/com/example/naunem/secondproject/RegisterActivity.java
@@ -4,10 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
@@ -31,7 +33,9 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     CheckBox cbSuftWeb;
     CheckBox cbCommic;
     Button btnRegister;
+    ImageView showpass;
     ArrayList<CheckBox> list = new ArrayList<>();
+    private int passwordNotVisible = 1;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -48,6 +52,8 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         cbCommic = (CheckBox) findViewById(R.id.cb_commic);
         btnRegister = (Button) findViewById(R.id.register);
         btnRegister.setOnClickListener(this);
+        showpass = (ImageView) findViewById(R.id.showpass);
+        showpass.setOnClickListener(this);
 
         list.add(cbFootball);
         list.add(cbListenMusic);
@@ -57,25 +63,38 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
 
     @Override
     public void onClick(View v) {
-        String gender = "";
-        if (rdMale.isChecked()){
-            gender = rdMale.getText().toString();
-        }else {
-            gender = rdFemale.getText().toString();
+        switch (v.getId()) {
+            case R.id.register:
+                String gender = "";
+                if (rdMale.isChecked()) {
+                    gender = rdMale.getText().toString();
+                } else {
+                    gender = rdFemale.getText().toString();
+                }
+
+                String hobby = "";
+                for (CheckBox item : list) {
+                    if (item.isChecked()) {
+                        hobby += item.getText().toString() + " ";
+                    }
+                }
+                String text = "";
+                text += "username: " + username.getText() + "\npass: " + pass.getText() + "\ngender: " + gender + "\nhobby: " + hobby;
+                Toast.makeText(this, "result: \n" + text, Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(this, LoginActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.showpass:
+                if(passwordNotVisible == 1) {
+                    pass.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                    passwordNotVisible = 0;
+                } else {
+                    pass.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    passwordNotVisible = 1;
+                }
+                pass.setSelection(pass.length());
+                break;
         }
-
-
-        String hobby = "";
-        for (CheckBox item : list){
-            if(item.isChecked()) {
-                hobby += item.getText().toString() + " ";
-            }
-        }
-        String text = "";
-        text += "username: " + username.getText() + "\npass: " + pass.getText() + "\ngender: " + gender + "\nhobby: " + hobby;
-        Toast.makeText(this, "result: \n" + text, Toast.LENGTH_SHORT).show();
-
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
     }
 }
